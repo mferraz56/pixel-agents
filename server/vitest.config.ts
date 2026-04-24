d96c239afe14d693569124cfa0ebc9493fc91db0
Murilo Ferraz <muriloferraz56@gmail.com>
2026-04-23 21:48:53 -0300

    clean all data

--- server/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    globals: true,
-    testTimeout: 10_000,
-    include: ['__tests__/**/*.test.ts'],
-  },
-});
